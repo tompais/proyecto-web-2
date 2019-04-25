3f19c67c5072d4f252aa47bed3f745d7eba6d0a1
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-04-24 21:43:52 -0300

    Subo base del proyecto

---
+++ Codigo Fuente/src/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Proyecto Web 2</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="src\wwwroot\lib\bootstrap\css\bootstrap.min.css">
+        <link rel="stylesheet" href="src\wwwroot\lib\daterangepicker\daterangepicker.css">
+        <link rel="stylesheet" href="src\wwwroot\lib\alertifyjs\css\alertify.min.css">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <script src="src\wwwroot\lib\jquery\jquery-3.4.0.min.js"></script>
+        <script src="src\wwwroot\lib\bootstrap\js\bootstrap.min.js"></script>
+        <script src="src\wwwroot\lib\popper\popper.min.js"></script>
+        <script src="src\wwwroot\lib\tooltip\tooltip.min.js"></script>
+        <script src="src\wwwroot\lib\moment\moment-with-locales.min.js"></script>
+        <script src="src\wwwroot\lib\daterangepicker\daterangepicker.js"></script>
+        <script src="src\wwwroot\lib\alertifyjs\alertify.min.js"></script>
+        <script src="src\wwwroot\lib\validate\validate.min.js"></script>
+    </body>
+</html>
